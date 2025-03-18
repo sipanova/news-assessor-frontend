7d7e1b34f5e43678f8d7bc6385f92e6d77b3b944
Sipan <sipan.hoj.7@gmail.com>
2025-03-18 15:28:05 +0100

    Add GUI components + and env folder.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,  // Standalone component,
+  imports: [RouterOutlet, CommonModule, FormsModule]
 })
 export class AppComponent {
-  title = 'news-assessor-frontend';
+  title = 'SwissRep';
+  selectedOption = 'GPT-4o'; // Default selected value
+  dropdownOptions = ['GPT-4o', 'Llama-3.2-3B-Instruct']; // Dropdown values
+
+  handleClick() {
+    console.log("Button clicked!");
+  }
+
+  handleFileUpload(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length === 1) {
+      const file = input.files[0];
+      if (file.type !== 'text/csv' && !file.name.endsWith('.csv')) {
+        alert('Please upload a valid CSV file.');
+        input.value = ''; // Reset file input
+        return;
+      }
+      console.log('Uploaded file:', file.name);
+    } else {
+      alert('Please upload only one CSV file.');
+      input.value = ''; // Reset file input
+    }
+  }
 }
+
+
+
